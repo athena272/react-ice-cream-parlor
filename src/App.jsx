5e0165432c5ab8a0474d9c9497c73f46b357abde
athena272 <guilhermera272@gmail.com>
2023-11-29 19:14:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Routespage from './Routes/RoutesPage';
 import './App.css';
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
 
 const App = () => (
   <div className='App'>
-    <Header />
     <Routespage />
-    <Footer />
   </div>
 );
 
