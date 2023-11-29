247b81534669d1e1bb1b042515b1cf1b77114a14
athena272 <guilhermera272@gmail.com>
2023-11-29 18:50:37 -0300

    update project

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,6 +1,6 @@
 import './AboutPage.css';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 const AboutPage = () => (
     <div>
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import './HomePage.css';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 const HomePage = () => (
     <div>
