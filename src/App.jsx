c03aa0494570fc0807541163fe5c7f600508bbdb
athena272 <guilhermera272@gmail.com>
2024-01-18 01:54:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import RoutesPage from './Routes/RoutesPage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 const App = () => (
   <div className='App'>
-    <RoutesPage />
+    <BrowserRouter> {/* Componente que armazena as rotas */}
+      <Header />
+      <RoutesPage />
+      <Footer />
+    </BrowserRouter>
   </div>
 );
 
